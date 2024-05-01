28a9fab61fce2d424be4c93a03fa632b990e5802
manu-roque <manu.roque3000@gmail.com>
2024-05-01 12:50:47 +0200

    fixing branch issues

---
+++ worklife_assignment/src/components/DetailsComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="details">
+    <h1>Title</h1>
+    <p>Description</p>
+    <button>test</button>
+  </div>
+</template>
+
+<script>
+export default {
+  //
+}
+</script>
+
+<style scoped>
+.details {
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background: white;
+  box-shadow: -2px 0 5px rgba(0, 0, 0, 0.5);
+  padding: 20px;
+}
+</style>
